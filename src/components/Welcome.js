1d6f6cf29d33834c8b3baf6b7406e4b8dcefe4b5
Salim-Simplon <amri.salim.simplon@gmail.com>
2020-06-25 17:39:31 +0100

    welcome page

---
+++ src/components/Welcome.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import './SignUp/SignUp.css';
+
+export class Welcome extends Component {
+  
+  
+  render() {
+    return (
+      <div>
+        <div className="Sin">
+          <div className="Siin">
+            <h1>BIENVENUR À NOTRE ATELIER
+                LITAL
+            </h1>
+            <p>Gestion des utilisateurs</p>
+            <p>Gestion de stock</p>
+            <p>Historique du mouvement du stock</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+export default Welcome 
